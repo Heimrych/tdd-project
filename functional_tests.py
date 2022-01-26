82589d4d859c146a0edab89d2e2a023818e98adb
Henrique Fagundes <hfagundes@pinterest.com>
2022-01-26 18:31:22 -0300

    Utilizando Testes Funcionais para planejar um MVP

--- functional_tests.py
+++ functional_tests.py
@@ -3,4 +3,25 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+assert 'To-Do' in browser.title
+
+# Ela é convidada a entrar com um item TODO imediatamente
+
+# Ela digita "Estudar testes funcionais" em uma caixa de texto
+
+# Quando ela aperta enter, a página atualiza, e mostra a lista
+# "1: Estudar testes funcionais" como um item da lista TODO
+
+# Ainda existe uma caixa de texto convidando para adicionar outro item
+# Ela digita: "Estudar testes de unidade"
+
+# A página atualiza novamente, e agora mostra ambos os itens na sua lista
+
+# Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
+# o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+
+# Ela visita a URL: a sua lista TODO ainda está armazenada
+
+# Satisfeita, ela vai dormir
+
+browser.quit()
